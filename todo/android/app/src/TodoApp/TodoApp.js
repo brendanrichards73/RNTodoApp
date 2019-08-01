b0be0d7b08d6306eec70b974568f69a3f7aafc14
brendan richards <brendan.richards@btinternet.com>
2019-08-01 09:52:57 +0100

    Second build of todoappscreen

--- todo/android/app/src/TodoApp/TodoApp.js
+++ todo/android/app/src/TodoApp/TodoApp.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import AddTodo from '../TodoApp/containers/AddTodo';
+import VisibleTodos from './containers/VisibleTodos';
+
 
 class TodoApp extends Component {
     render () {
         return (
-            <View style={StyleSheet.container}>
+            <View style={styles.container}>
                 <AddTodo />
+
+                <View>
+                    <VisibleTodos />
+                </View>
             </View>
         )
     }
@@ -16,5 +22,6 @@ export default TodoApp;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        paddingTop: 40
     }
 })
