2ae632dacce1f41ce2bc5f687492815425188e73
brendan richards <brendan.richards@btinternet.com>
2019-08-01 10:30:42 +0100

    finished AddTodo -  now works

--- todo/android/app/src/TodoApp/containers/AddTodo.js
+++ todo/android/app/src/TodoApp/containers/AddTodo.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { View, TextInput, StyleSheet, TouchableOpacity } from 'react-native';
 import AntIcon from 'react-native-vector-icons/AntDesign'; 
 import { connect } from 'react-redux';
-import addTodo from '../actions/index';
+import {addTodo} from '../actions/index';
 
 class AddTodo extends Component {
 
@@ -10,7 +10,7 @@ class AddTodo extends Component {
         text:''
     }
 
-    AddTodo = (text) =>{
+    addTodo = (text) => {
         this.props.dispatch(addTodo(text))
         this.setState({ text: '' })
     }
@@ -19,13 +19,13 @@ class AddTodo extends Component {
         return (
            <View style={{ flexDirection: 'row'}}>
                <TextInput
-                    onChangeText={(text) => this.setState({text})} 
+                    onChangeText={(text) => this.setState({ text })} 
                     value={this.state.text}
-                   placeholder='Eg, Create New Video'
+                   placeholder='Enter New Todo'
                    style={{borderWidth: 1, borderColor: '#f2f2e1',
                    backgroundColor: '#eaeaea', height: 50, flex: 1, padding: 5}}
                />
-               <TouchableOpacity onPress={(text) => this.setState({ text })}>
+               <TouchableOpacity onPress={() => this.addTodo(this.state.text)}>
                    <View style={{ height: 50, backgroundColor: '#eaeaea', alignItems:'center', justifyContent: 'center'}}>
                        <AntIcon name='plus' size={30} style={{ color: '#de9595', padding: 10}} />
                    </View>
--- todo/android/app/src/TodoApp/reducers/todos.js
+++ todo/android/app/src/TodoApp/reducers/todos.js
@@ -3,7 +3,7 @@ const todos = (state=[], action) => {
     switch(action.type){
         case 'ADD_TODO':
             return [
-                ...state,{
+                ...state, {
                     id: action.id,
                     text: action.text,
                     completed: false
