2e296fada75be1e687fba0532d02335b2b4add7b
brendan richards <brendan.richards@btinternet.com>
2019-08-08 16:32:40 +0100

    Moved src JS to top level

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
  'use strict';
 
  import React, { Component } from 'react';
- import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
  import { createStackNavigator, createAppContainer } from 'react-navigation';
- import API from './android/app/src/Screens/API';
- import Home from './android/app/src/Screens/Home';
- import ToDo from './android/app/src/Screens/ToDo';
- import Images from './android/app/src/Screens/Images';
+ import API from './src/Screens/API';
+ import Home from './src/Screens/Home';
+ import ToDo from './src/Screens/ToDo';
+ import Images from './src/Screens/Images';
  
 
  const Navigation = createStackNavigator({
